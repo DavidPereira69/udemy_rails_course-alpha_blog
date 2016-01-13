c320e7047a88cd418db55e6b0507fb4f19e71abd
DavidPereira69 <dpereira@machfive.net>
2016-01-13 17:13:58 +0000

    Added new views.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,8 +7,18 @@ class ArticlesController < ApplicationController
   def create
     #render plain: params[:article].inspect #to display the hash/passed variables of the form in browser
     @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
+  
+    if @article.save
+      flash[:notice] = "Your article was created."
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+    
+  end
+  
+  def show
+    @article = Article.find(params[:id])
   end
   
   private
